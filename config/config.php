4853cc90e8801100cdc603857260887a085dc684
kennethEdwin147 <edwingbeti@gmail.com>
2022-07-13 20:29:15 -0400

    mini-mvc base

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+// URL de base du serveur
+const BASE_SERVEUR = '';
+
+// Config de la BD
+const BD_HOTE = 'localhost';
+const BD_UTIL = '';
+const BD_MDP = '';
+const BD_NOM = '';
---
+++ controleurs/NouveauControleur.cls.php
@@ -0,0 +1,13 @@
+<?php
+class NouveauControleur extends Controleur
+{
+    /**
+     * Méthode invoquée par défaut si aucune action n'est indiquée
+     */
+    public function index($params)
+    {
+        //$this->gabarit->affecterActionParDefaut('tout');
+        //$this->tout($params);
+
+    }
+}
